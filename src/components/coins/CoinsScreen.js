64f40ff684c007fc01ebfcbd0ab90dc4d37f8d7a
Hugo Moreira <imagetech.developer07@outlook.com>
2020-11-25 14:25:33 -0500

    First commit

---
+++ src/components/coins/CoinsScreen.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import { View, FlatList, StyleSheet, ActivityIndicator } from 'react-native';
+import Http from "cryptoTracker/src/libs/http";
+import CoinsItem from './CoinsItem';
+import Colors from "cryptoTracker/src/res/colors";
+
+class CoinsScreen extends Component {
+    state = {
+        coins: []
+    }
+
+    componentDidMount = async () => {
+        this.setState({loading:true});
+        const res = await Http.instance.get("https://api.coinlore.net/api/tickers/");
+        this.setState({ coins: res.data, loading:false });
+    }
+
+    handlePress = (coin) => {
+        this.props.navigation.navigate('CoinDetail',{coin});
+    }
+
+    render() {
+        const {coins, loading} = this.state;
+
+        return (
+            <View style={styles.container} >
+                { loading?
+                    <ActivityIndicator
+                        style={styles.loader}
+                        color="#fff"
+                        size="large"
+                        />
+                    : null
+                }
+
+                <FlatList
+                    data={coins}
+                    renderItem={({ item }) =>
+                    <CoinsItem
+                        item={item}
+                        onPress={()=> this.handlePress(item)}
+                    />
+                   
+                }
+                />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: Colors.charade
+    },
+    titleText: {
+        backgroundColor: "#fff",
+        textAlign: "center"
+    },
+    btn: {
+        padding: 8,
+        backgroundColor: "blue",
+        borderRadius: 8,
+        margin: 16
+    },
+    btnText: {
+        color: "#fff",
+        textAlign: "center"
+    },
+    loader:{
+        marginTop: 60
+    }
+
+});
+
+
+export default CoinsScreen;
